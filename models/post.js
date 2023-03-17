fe51a2b2a28289ad237fd06bb6fc18e58c71a750
Rif-7 <muhammadrifan974530@gmail.com>
2023-03-17 22:31:10 +0530

    create post schema

---
+++ models/post.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, minLength: 3, maxLength: 150, required: true },
+  timestamp: { type: Date, default: Date.now, required: true },
+});
+
+PostSchema.virtual("timestamp_formatted").get(function () {
+  return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATE_MED);
+});
+
+module.exports = mongoose.model("Post", PostSchema);
